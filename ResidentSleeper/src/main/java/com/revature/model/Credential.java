c0737e3bc66bb38108c610a70034cfed02b76400
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-09 13:49:26 -0400

    seperated the credentials data from the user table into it's own class

---
+++ ResidentSleeper/src/main/java/com/revature/model/Credential.java
@@ -0,0 +1,129 @@
+package com.revature.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CREDENTIAL")
+public class Credential {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "credentialSequence")
+	@SequenceGenerator(allocationSize = 1, name = "flashcardSequence", sequenceName = "SQ_CREDENTIAL_PK")
+	@Column(name = "CREDENTIAL_ID")
+	private int credentialId; // primary key for the credential table
+	@Column(name = "USERNAME")
+	private String username; // used to login- username field
+	@Column(name = "PASSWORD")
+	private String password; // used to login- password field
+	@OneToOne
+	@JoinColumn(name = "USER_ID")
+	private User user; // tying the user table to the credential table via userId
+
+	// generated getters, setters, hashcode, equals, toString, constructors
+
+	public int getCredentialId() {
+		return credentialId;
+	}
+
+	public void setCredentialId(int credentialId) {
+		this.credentialId = credentialId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + credentialId;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Credential other = (Credential) obj;
+		if (credentialId != other.credentialId)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Credential [credentialId=" + credentialId + ", username=" + username + ", password=" + password
+				+ ", user=" + user + "]";
+	}
+
+	public Credential(int credentialId, String username, String password, User user) {
+		super();
+		this.credentialId = credentialId;
+		this.username = username;
+		this.password = password;
+		this.user = user;
+	}
+
+	public Credential(String username, String password, User user) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.user = user;
+	}
+
+	public Credential() {
+		super();
+	}
+
+}
--- ResidentSleeper/src/main/java/com/revature/model/User.java
+++ ResidentSleeper/src/main/java/com/revature/model/User.java
@@ -3,13 +3,153 @@ package com.revature.model;
 public class User {
 
 	private int userId; // primary key for User table
-	private String username; // used to login- username field
-	private String password; // used to login- password field
 	private String email;
 	private String firstname;
 	private String lastname;
 	private String avatarFilename; // the filename of the users avatar image
 	private int coins; // amount of coins saved up from playing games
 	private int minutes; // total time spent playing games in minutes
+
+	// generated getters, setters, hashcode, equals, toString, constructors
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAvatarFilename() {
+		return avatarFilename;
+	}
+
+	public void setAvatarFilename(String avatarFilename) {
+		this.avatarFilename = avatarFilename;
+	}
+
+	public int getCoins() {
+		return coins;
+	}
+
+	public void setCoins(int coins) {
+		this.coins = coins;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((avatarFilename == null) ? 0 : avatarFilename.hashCode());
+		result = prime * result + coins;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + minutes;
+		result = prime * result + userId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (avatarFilename == null) {
+			if (other.avatarFilename != null)
+				return false;
+		} else if (!avatarFilename.equals(other.avatarFilename))
+			return false;
+		if (coins != other.coins)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (minutes != other.minutes)
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", email=" + email + ", firstname=" + firstname + ", lastname=" + lastname
+				+ ", avatarFilename=" + avatarFilename + ", coins=" + coins + ", minutes=" + minutes + "]";
+	}
+
+	public User(int userId, String email, String firstname, String lastname, String avatarFilename, int coins,
+			int minutes) {
+		super();
+		this.userId = userId;
+		this.email = email;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.avatarFilename = avatarFilename;
+		this.coins = coins;
+		this.minutes = minutes;
+	}
+
+	public User(String email, String firstname, String lastname, String avatarFilename, int coins, int minutes) {
+		super();
+		this.email = email;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.avatarFilename = avatarFilename;
+		this.coins = coins;
+		this.minutes = minutes;
+	}
+
+	public User() {
+		super();
+	}
 
 }
