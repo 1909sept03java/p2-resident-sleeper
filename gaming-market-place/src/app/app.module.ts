8cb86aac3faabdf1b658b4794797d8b63b7849ff
unknown <gurvir14128@gmail.com>
2019-10-09 19:40:49 -0400

    Initial Login Page Added

--- gaming-market-place/src/app/app.module.ts
+++ gaming-market-place/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { CustomMaterialModule } from  './material.module';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
