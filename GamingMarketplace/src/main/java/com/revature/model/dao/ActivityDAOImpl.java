4deec5294fccb87e6bafba1b390e295d4ee98ed2
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-10 10:16:53 -0400

    created implementation classes with the blank methods overriding the interfaces

---
+++ GamingMarketplace/src/main/java/com/revature/model/dao/ActivityDAOImpl.java
@@ -0,0 +1,39 @@
+package com.revature.model.dao;
+
+import java.util.List;
+
+import com.revature.model.bean.Activity;
+
+public class ActivityDAOImpl implements ActivityDAO {
+
+	@Override
+	public List<Activity> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Activity getActivityById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ GamingMarketplace/src/main/java/com/revature/model/dao/PlayerItemDAOImpl.java
@@ -0,0 +1,39 @@
+package com.revature.model.dao;
+
+import java.util.List;
+
+import com.revature.model.bean.PlayerItem;
+
+public class PlayerItemDAOImpl implements PlayerItemDAO {
+
+	@Override
+	public List<PlayerItem> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PlayerItem getPlayerItemById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPlayerItem(PlayerItem playerItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePlayerItem(PlayerItem playerItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePlayerItem(PlayerItem playerItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
