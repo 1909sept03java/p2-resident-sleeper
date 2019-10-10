f126daacfc3733de3dd0d581300ab054ef3a783c
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-10 00:55:06 -0400

    starting on the dao interfaces

--- GamingMarketplace/src/main/java/com/revature/model/dao/CredentialDAO.java
+++ GamingMarketplace/src/main/java/com/revature/model/dao/CredentialDAO.java
@@ -1,5 +1,28 @@
 package com.revature.model.dao;
 
+import java.util.List;
+
+import com.revature.model.bean.Credential;
+import com.revature.model.bean.Player;
+
 public interface CredentialDAO {
 
+	// this method returns a list of all credential's
+	public List<Credential> getAll();
+	
+	// this method returns a row in credential
+	public Credential getCredentialById(int credentialId);
+	
+	// this method returns a row in player by credential id
+	public Player getPlayerById(int credentialId);
+	
+	// this method adds a row into the credential table
+	public void addCredential(Credential credential);
+	
+	// this method updates a row in the credential table
+	public void updateCredential(Credential credential);
+	
+	// this method deletes a row in the credential table
+	public void deleteCredential(Credential credential);
+	
 }
--- GamingMarketplace/src/main/java/com/revature/model/dao/PlayerDAO.java
+++ GamingMarketplace/src/main/java/com/revature/model/dao/PlayerDAO.java
@@ -1,5 +1,23 @@
 package com.revature.model.dao;
 
+import java.util.List;
+
+import com.revature.model.bean.Player;
+
 public interface PlayerDAO {
 
+	// this method returns a list of all player's
+	public List<Player> getAll();
+	
+	// this method returns a row in the player table
+	public Player getPlayerById();
+
+	// this method adds a row into the player table
+	public void addPlayer(Player player);
+
+	// this method updates a row in the player table
+	public void updatePlayer(Player player);
+
+	// this method deletes a row in the player table
+	public void deletePlayer(Player player);
 }
