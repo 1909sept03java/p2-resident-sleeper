d10c2bbc74c4a4b5a160790a2d9ea81b6afef848
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-10 10:42:55 -0400

    working on dao implementations

--- GamingMarketplace/src/main/java/com/revature/model/dao/ActivityDAO.java
+++ GamingMarketplace/src/main/java/com/revature/model/dao/ActivityDAO.java
@@ -5,20 +5,20 @@ import java.util.List;
 import com.revature.model.bean.Activity;
 
 public interface ActivityDAO {
-	
+
 	// this method returns a list of all player's
-		public List<Activity> getAll();
-		
-		// this method returns a row in the player table
-		public Activity getActivityById();
+	public List<Activity> getAll();
 
-		// this method adds a row into the player table
-		public void addActivity(Activity activity);
+	// this method returns a row in the player table
+	public Activity getActivityById();
 
-		// this method updates a row in the player table
-		public void updateActivity(Activity activity);
+	// this method adds a row into the player table
+	public void addActivity(Activity activity);
 
-		// this method deletes a row in the player table
-		public void deleteActivity(Activity activity);
+	// this method updates a row in the player table
+	public void updateActivity(Activity activity);
+
+	// this method deletes a row in the player table
+	public void deleteActivity(Activity activity);
 
 }
---
+++ GamingMarketplace/src/main/java/com/revature/model/dao/PlayerDAOImpl.java
@@ -0,0 +1,39 @@
+package com.revature.model.dao;
+
+import java.util.List;
+
+import com.revature.model.bean.Player;
+
+public class PlayerDAOImpl implements PlayerDAO {
+
+	@Override
+	public List<Player> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player getPlayerById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
