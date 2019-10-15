6cf191ae7f09edd1b4c17ffbac7aac49ed5369a1
unknown <gurvir14128@gmail.com>
2019-10-15 15:15:00 -0400

    Added login service

--- gaming-market-place/src/app/game/game.constants.ts
+++ gaming-market-place/src/app/game/game.constants.ts
@@ -1,13 +1,13 @@
-export const BOARD_SIZE = 18;
+export const BOARD_SIZE = 18; // size of the board
 
-export const CONTROLS = {
+export const CONTROLS = { // buttons for controlling snake movement
   LEFT: 37,
   UP: 38,
   RIGHT: 39,
   DOWN: 40
 };
 
-export const COLORS = {
+export const COLORS = { // colors of the game.
   GAME_OVER: '#D24D57',
   FRUIT: '#EC644B',
   HEAD: '#336E7B',
@@ -16,7 +16,7 @@ export const COLORS = {
   OBSTACLE: '#383522'
 };
 
-export const GAME_MODES = {
+export const GAME_MODES = { // 3 different game modes
   classic: 'Classic',
   no_walls: 'No Walls',
   obstacles: 'Obstacles'
