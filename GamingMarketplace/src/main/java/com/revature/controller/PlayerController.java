54f162d350769b366d59332e9ef44fbbcc01004b
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-15 20:28:42 -0400

    added player/id

--- GamingMarketplace/src/main/java/com/revature/controller/PlayerController.java
+++ GamingMarketplace/src/main/java/com/revature/controller/PlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,6 +29,17 @@ public class PlayerController {
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ResponseEntity<List<Player>> getAll() {
 		return new ResponseEntity<>(this.playerService.getAll(), HttpStatus.OK);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Player> getPlayerById(@PathVariable int id) {
+		Player player = this.playerService.getPlayerById(id);
+		if (player == null) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(player, HttpStatus.OK);
+		}
 	}
 
 }
