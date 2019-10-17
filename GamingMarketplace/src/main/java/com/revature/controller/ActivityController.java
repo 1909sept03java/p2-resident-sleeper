058420b676ae32932e7ed57bc97230b913ee87f3
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-17 13:21:38 -0400

    took off logging

--- GamingMarketplace/src/main/java/com/revature/controller/ActivityController.java
+++ GamingMarketplace/src/main/java/com/revature/controller/ActivityController.java
@@ -2,7 +2,6 @@ package com.revature.controller;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,19 +16,17 @@ import com.revature.service.ActivityService;
 @Controller
 @RequestMapping(value = "/activity")
 public class ActivityController {
-	
+
 	private ActivityService activityService;
-	private static Logger log = Logger.getRootLogger();
-	
+
 	@Autowired // setter injection
 	public void setActivityService(ActivityService activityService) {
 		this.activityService = activityService;
 	}
-	
+
 	@ResponseBody // tells spring to skip ViewResolver
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ResponseEntity<List<Activity>> getAll() {
-		log.info("Someone called the activity/all endpoint");
 		return new ResponseEntity<>(this.activityService.getAll(), HttpStatus.OK);
 	}
 
