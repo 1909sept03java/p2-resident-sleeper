f37be486555508fdd7c6a087dbdfa57bb29f65b3
tadegbenro <tomi.adegbenro@gmail.com>
2019-10-18 17:33:59 -0400

    JUnit test for CredentialDAOImpl Test

--- GamingMarketplace/src/test/java/com/revature/dao/CredentialDAOImplTest.java
+++ GamingMarketplace/src/test/java/com/revature/dao/CredentialDAOImplTest.java
@@ -1,34 +1,65 @@
 package com.revature.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.revature.OrmConfiguration;
+import com.revature.model.Credential;
+import com.revature.model.Player;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = OrmConfiguration.class)
 public class CredentialDAOImplTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	private CredentialDAO credentialDAO;
+	
+	private Credential cred;
+	
+	private Player player;
+	
+	@Autowired
+	public void setCredentialDAO(CredentialDAO credentialDAO) {
+		this.credentialDAO = credentialDAO;
 	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	
+	@Autowired
+	public void setCredential(Credential cred) {
+		this.cred = cred;
 	}
-
-	@Before
-	public void setUp() throws Exception {
+	
+	@Autowired
+	public void setplayer(Player player) {
+		this.player = player;
 	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		System.out.println(credentialDAO);
+		//setup expected player
+		player.setplayerId(1);
+		player.setFirstname("rex");
+		player.setLastname("cruz");
+		player.setEmail("rex@example.com");
+		player.setCoins(100);
+		player.setAvatarFilename("helmet.jpg");
+		player.setMinutes(0);
+		//setup expected Credential
+		cred.setUsername("rex13");
+		cred.setCredentialId(1);
+		cred.setPassword("rexpass");
+		cred.setPlayer(player);
+		
+		System.out.println(cred);
+		assertEquals(cred, credentialDAO.getCredentialById(1));
+		
+		//fail("Not yet implemented");
 	}
 
 }
