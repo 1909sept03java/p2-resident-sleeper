040162b5e59cf3e1fa23331c71a02042a99f2998
tadegbenro <tomi.adegbenro@gmail.com>
2019-10-20 13:23:56 -0400

    Junit tesitng of Credential artifacts 2

--- GamingMarketplace/src/test/java/com/revature/controller/CredentialControllerTest.java
+++ GamingMarketplace/src/test/java/com/revature/controller/CredentialControllerTest.java
@@ -1,34 +1,31 @@
 package com.revature.controller;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.revature.OrmConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = OrmConfiguration.class)
 public class CredentialControllerTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+	private CredentialController credentialController;
+	
+	@Autowired
+	public void setCredentialController(CredentialController credentialController) {
+		this.credentialController = credentialController;
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertThat(credentialController, instanceOf(CredentialController.class));
+		//fail("Not yet implemented");
 	}
 
 }
